fe7c74a090a8c90df78242b398a995007f628f68
Sagar <sagar.devices@gmail.com>
2023-03-07 08:35:27 +0530

    first service under controller

--- src/services/api/hiveuser/hiveuser.controller.ts
+++ src/services/api/hiveuser/hiveuser.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get, Param } from '@nestjs/common'
+import { Controller, Get, Param, Res } from '@nestjs/common'
 import { HiveuserService } from './hiveuser.service';
+import {Response} from 'express';
 
 @Controller('/api/v1/hiveuser')
 export class HiveuserController {
   constructor(private readonly hiveuserService: HiveuserService) {}
 
   @Get('/getInfo/:username')
-  getInfo(@Param('username') username: string) {
-    return this.hiveuserService.findOne(username);
+  async getInfo(@Param('username') username: string, @Res() res: Response) {
+    const value = await this.hiveuserService.findOne(username);
+    console.log(value);
+    if (value === undefined || value === null) {
+      return res.status(404).send({ message: 'No such hive user found.' });
+    }
+    return res.send(value);
   }
 }
