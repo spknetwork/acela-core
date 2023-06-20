f6d930eae381ed0798af3d4c56d3cf68edfc9754
vaultec <47548474+vaultec81@users.noreply.github.com>
2023-06-19 21:20:32 -0700

    fix: possible issue with escape mongodb info

--- src/services/db.ts
+++ src/services/db.ts
@@ -6,7 +6,7 @@ import 'dotenv/config'
 const CORE_MONGODB_URL = process.env.CORE_MONGODB_URL || '127.0.0.1:27017'
 
 export const MONGODB_URL = `mongodb://${CORE_MONGODB_URL}`
-export const mongo = new MongoClient(MONGODB_URL)
+export const mongo = new MongoClient(CORE_MONGODB_URL)
 
 const Users = new Schema({})
 
