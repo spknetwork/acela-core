8a3933bf115ec0ee556f12b3f493df3e2f56097c
vaultec <47548474+vaultec81@users.noreply.github.com>
2023-06-19 21:15:07 -0700

    tweak: use CORE_MONGODB_URL

--- src/services/db.ts
+++ src/services/db.ts
@@ -1,8 +1,15 @@
 import { MongoClient } from 'mongodb'
+import { Schema } from 'mongoose'
 import 'dotenv/config'
 
 
-const MONGO_HOST = process.env.MONGO_HOST || '127.0.0.1:27017'
+const CORE_MONGODB_URL = process.env.CORE_MONGODB_URL || '127.0.0.1:27017'
 
-export const MONGODB_URL = `mongodb://${MONGO_HOST}`
+export const MONGODB_URL = `mongodb://${CORE_MONGODB_URL}`
 export const mongo = new MongoClient(MONGODB_URL)
+
+const Users = new Schema({})
+
+Users.index({
+    username: -1
+})
