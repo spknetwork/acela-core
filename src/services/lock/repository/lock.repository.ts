cb2a9c52c58ea4e5e8237f0d1e551c64907dfdfb
Christopher Graney-Ward <chris@paritae.com>
2024-03-06 21:29:09 +0000

    Complete lock service in nest, integrate to tasks module

--- src/services/lock/repository/lock.repository.ts
+++ src/services/lock/repository/lock.repository.ts
@@ -10,25 +10,43 @@ export class LockRepository {
     @InjectModel(Lock.name, 'acela-core') private readonly lockModel: Model<Lock>,
   ) { }
 
-  async #findOneByIdAndUpdateRegisteredId({ id, registered_id }, shouldCreate: boolean) {
+  async #findOneByIdAndUpdateRegisteredId({
+    id,
+    registered_id,
+    registered_ping = new Date()
+  }: {
+    id: string;
+    registered_id: string | null;
+    registered_ping?: Date | null;
+  },
+    shouldCreate: boolean
+  ) {
     return await this.lockModel.findOneAndUpdate({
       id
     }, {
       $set: {
         registered_id,
-        registered_ping: new Date()
+        registered_ping
       }
     }, {
       upsert: shouldCreate
     })
   }
 
-  async findOneByIdAndUpdateRegisteredIdOrIgnore({ id, registered_id }: { id: string, registered_id: string }) {
-    return await this.#findOneByIdAndUpdateRegisteredId({ id, registered_id }, false);
+  async findOneByIdAndUpdateRegisteredIdOrIgnore({ id, registered_id, registered_ping }: { id: string, registered_id: string, registered_ping?: null }) {
+    return await this.#findOneByIdAndUpdateRegisteredId({ id, registered_id, registered_ping }, false);
   }
 
-  async findOneByIdAndUpdateRegisteredIdOrCreate({ id, registered_id }: { id: string, registered_id: string }) {
-    return await this.#findOneByIdAndUpdateRegisteredId({ id, registered_id }, true);
+  async findOneByIdAndUpdateRegisteredIdOrCreate({ id, registered_id, registered_ping }: { id: string, registered_id: string, registered_ping?: null }) {
+    return await this.#findOneByIdAndUpdateRegisteredId({ id, registered_id, registered_ping }, true);
+  }
+
+  async createOrThrowIfExistingLock(data: { id: string; registered_id: string; }) {
+    const existingDocument = await this.findOneById(data.id);
+    if (existingDocument) {
+      throw new Error('Document with the specified unique criteria already exists.');
+    }
+    return await this.lockModel.create(data);
   }
 
   async findOneById(id: string) {
@@ -42,7 +60,7 @@ export class LockRepository {
   async fetchServicesLastPingedBefore10Minutes() {
     return await this.lockModel.find({
       registered_ping: {
-          $lt: moment().subtract('10', 'minute').toDate()
+        $lt: moment().subtract('10', 'minute').toDate()
       }
     });
   }
--- src/services/tasks/tasks.module.ts
+++ src/services/tasks/tasks.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PublishingModule } from '../publishing/publishing.module';
 import { TasksService } from './tasks.service';
+import { LockModule } from '../lock/lock.module';
+import { LockService } from '../lock/service/lock.service';
 
 @Module({
-  imports: [PublishingModule],
+  imports: [PublishingModule, LockModule],
   providers: [TasksService],
 })
 export class TasksModule {}
